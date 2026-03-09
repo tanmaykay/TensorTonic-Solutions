705cc56db6538844c161e33f4debbde2dab77d97
tanmaykay <124807656+tanmaykay@users.noreply.github.com>
2026-03-09 19:59:36 +0530

    Add solution for sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Vectorized sigmoid function.
+    """
+    x=np.asarray(x, dtype=float)
+    # print(vector)
+    return 1/(1+np.exp(-x))
+    pass
